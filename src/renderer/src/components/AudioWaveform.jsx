02928742287b6f6c38c999e8c8a338bbfdff950a
Peter Soava <thelingo56@gmail.com>
2023-09-29 20:04:50 -0700

    add sample rate and count display

--- src/renderer/src/components/AudioWaveform.jsx
+++ src/renderer/src/components/AudioWaveform.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 const AudioWaveform = ({ audioUrl }) => {
   const leftCanvasRef = useRef(null)
   const rightCanvasRef = useRef(null)
+  const audioInfoRef = useRef(null)
 
   useEffect(() => {
     const leftCanvas = leftCanvasRef.current
@@ -16,6 +17,14 @@ const AudioWaveform = ({ audioUrl }) => {
       try {
         const response = await fetch(audioUrl)
         const arrayBuffer = await response.arrayBuffer()
+
+        // Get sample rate
+        const view = new DataView(arrayBuffer)
+        const byteOffset = 24 // Offset for the sample rate field in the header
+        const littleEndian = true // WAV files use little-endian byte order
+        const sampleRate = view.getUint32(byteOffset, littleEndian)
+
+        // Produce audio buffer that can be read into a waveform
         const audioContext = new (window.AudioContext || window.webkitAudioContext)()
         const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
 
@@ -34,10 +43,15 @@ const AudioWaveform = ({ audioUrl }) => {
         const scaleFactor = leftCanvas.width / audioData.length
 
         // The number of times to sample the audio file
-        const sampleCount = 5000
+        const sampleCount = audioData.length
 
         // How large the gap between samples should be
-        const sampleSize = Math.ceil(audioData.length / sampleCount)
+        let sampleSize = 1
+
+        // If the sample count is large put a cap when drawing waveform (assignment asks for 65536)
+        if (sampleCount > 65536) {
+          sampleSize = Math.ceil(sampleCount / 65536)
+        }
 
         // Draw left channel (Channel 0) on the first canvas
         leftContext.strokeStyle = 'white'
@@ -72,6 +86,8 @@ const AudioWaveform = ({ audioUrl }) => {
         }
 
         rightContext.stroke()
+
+        audioInfoRef.current.innerHTML = `File Sample Count: <span style="color: white">${sampleCount}</span> <br> File Sample Frequency: <span style="color: white">${sampleRate} hz</span>`
       } catch (error) {
         console.error('Error fetching audio data:', error)
       }
@@ -86,6 +102,7 @@ const AudioWaveform = ({ audioUrl }) => {
       <canvas ref={leftCanvasRef} width={800} height={150} />
       <h2 style={{ margin: '0', verticalAlign: 'top' }}>Right Channel</h2>
       <canvas ref={rightCanvasRef} width={800} height={150} />
+      <div ref={audioInfoRef} style={{ margin: '10px', verticalAlign: 'top' }}></div>
     </div>
   )
 }
